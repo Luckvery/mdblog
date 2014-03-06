2f15a1f8bde609de0fab7f0eb2e2b28518e6126f
Russell John-Baptiste <luckvery@gmial.com>
2014-03-06 12:51:10 -0500

    initial demo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mdblog::Application.config.secret_key_base = 'c478f1fdb1507d030da21ae334960e519f72e23a865e0302c282e90fb45bc810d975d1fe9257385c3fe0745f8bfafb249853f7962897873325aea56ec27f0aa7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mdblog::Application.config.session_store :cookie_store, key: '_mdblog_session'
