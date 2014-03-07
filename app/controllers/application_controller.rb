ff962b915be3cef484b691b3a9eaeaa44a0e4ed5
Russell John-Baptiste <luckvery@gmial.com>
2014-03-07 02:19:07 -0500

    conditional comment change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  browser = Browser.new(:ua => "some string", :accept_language => "en-us")
 end
